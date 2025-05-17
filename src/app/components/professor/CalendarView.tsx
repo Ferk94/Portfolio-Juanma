232e9419659818d388a1c521d95c3ad46481045e
Ferk94 <fernando.kaganovicz@gmail.com>
2025-05-17 05:35:42 -0300

    fix: se comentó el apartado calendario para primer deploy

--- src/app/components/professor/CalendarView.tsx
+++ src/app/components/professor/CalendarView.tsx
@@ -1,29 +1,29 @@
-"use client";
+// "use client";
 
-import { useState, useEffect } from "react";
-import 'react-calendar/dist/Calendar.css'; // Estilos por defecto
-import styles from './CalendarView.module.css';
-import dynamic from "next/dynamic";
+// import { useState, useEffect } from "react";
+// import 'react-calendar/dist/Calendar.css'; // Estilos por defecto
+// import styles from './CalendarView.module.css';
+// import dynamic from "next/dynamic";
 
-export default function CalendarView() {
-  const [date, setDate] = useState(new Date());
-   const [mounted, setMounted] = useState(false);
-  const Calendar = dynamic(() => import("react-calendar"), { ssr: false });
+// export default function CalendarView() {
+//   const [date, setDate] = useState(new Date());
+//    const [mounted, setMounted] = useState(false);
+//   const Calendar = dynamic(() => import("react-calendar"), { ssr: false });
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+//   useEffect(() => {
+//     setMounted(true);
+//   }, []);
 
-  if (!mounted) return null;
+//   if (!mounted) return null;
 
-  return (
-    <div className={styles.calendarContainer}>
-      <h2 className={styles.title}>Calendario de Clases</h2>
-      <Calendar onChange={setDate} value={date} />
-      <p className={styles.selectedDate}>
-        Fecha seleccionada: {date.toLocaleDateString()}
-      </p>
-    </div>
-  );
-}
+//   return (
+//     <div className={styles.calendarContainer}>
+//       <h2 className={styles.title}>Calendario de Clases</h2>
+//       <Calendar onChange={setDate} value={date} />
+//       <p className={styles.selectedDate}>
+//         Fecha seleccionada: {date.toLocaleDateString()}
+//       </p>
+//     </div>
+//   );
+// }
 
